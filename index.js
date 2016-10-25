b58cf7642a1429fc1e01b53d34e515f757cbd18d
Andrews Medina <andrewsmedina@gmail.com>
2016-10-25 19:36:32 -0200

    saving session

--- index.js
+++ index.js
@@ -1,16 +1,34 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var session = require('express-session');
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded());
+app.use(session({
+  secret: 'some secret',
+  resave: false,
+  saveUninitialized: true,
+}));
 
 app.get('/login', function(req, res){
   res.sendFile(__dirname + '/login.html');
 });
 
 app.post('/login', function(req, res) {
+  var users = req.session.users;
+
+  if (!users) {
+    users = req.session.users = [];
+  }
+
+  users.push(req.body.username);
+
   res.redirect('/chat')
 });
 
 app.get('/chat', function(req, res){
+  console.log("users 2:", req.session.users);
   res.sendFile(__dirname + '/index.html');
 });
 
