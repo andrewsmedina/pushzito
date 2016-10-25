b7ce19b6016754b27dcba822514fe75cda4279a5
Andrews Medina <andrewsmedina@gmail.com>
2016-10-24 23:58:26 -0200

    get users from mongo

--- pushzito/app.py
+++ pushzito/app.py
@@ -1,9 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask.ext.pymongo import PyMongo
+
+
 app = Flask(__name__)
+mongo = PyMongo(app)
+
 
 @app.route("/")
 def index():
-    return "index"
+    users = mongo.db.users.find()
+    return render_template('index.html', users=users)
+
 
 if __name__ == "__main__":
     app.run()
